1d623668826f0668f9bf322218cc966d689acbcd
KrainovSD <denislosev48@gmail.com>
2023-08-12 15:23:29 +0300

    Core: switch lerna to yarn workspaces

--- packages/site/pages/index.tsx
+++ packages/site/pages/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import HeaderContainer from '../components/HeaderContainer/HeaderContainer'
 import styles from './index.module.scss'
-import { typings } from 'common/utils'
+import { typings, node } from 'common/utils'
 
 const Index = () => {
   React.useEffect(() => {
     console.log(typings.isString('test'))
     console.log(typings.isString(''))
+    console.log(node.genUUID())
 
     console.log('test')
     console.log(process.env.NEXT_PUBLIC_TEST)
